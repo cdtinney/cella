b2e780e1af23aadf40db6d948daa6901749dab7a
Colin Tinney <colin_tinney@carleton.ca>
2013-04-07 17:04:42 -0400

    colours added for different cell states in edit rules

--- framework/public/code/editRules.js
+++ framework/public/code/editRules.js
@@ -5,11 +5,13 @@ function addRule()
 	var ddlSurround = document.createElement('select');
 	var surroundButton = document.createElement('button');
 	var resultButton = document.createElement('button');
+
 	$(div).attr('class', 'divRule');
 	$(targetButton).attr('id', 'btnTarget');
 	$(ddlSurround).attr('id', 'ddlSurround');
 	$(surroundButton).attr('id', 'btnSurround');
 	$(resultButton).attr('id', 'btnResult');
+	
 	$(div).append(targetButton);
 	$(div).append(ddlSurround);
 	$(div).append(surroundButton);
@@ -41,7 +43,35 @@ function toggleButtonCell(btn)
 	{
 		$(btn).attr('class', 'state1');
 	}
-	else
+	else if ($(btn).attr('class') == 'state1')
+	{
+		$(btn).attr('class', 'state2');
+	}
+	else if ($(btn).attr('class') == 'state2')
+	{
+		$(btn).attr('class', 'state3');
+	}
+	else if ($(btn).attr('class') == 'state3')
+	{
+		$(btn).attr('class', 'state4');
+	}
+	else if ($(btn).attr('class') == 'state4')
+	{
+		$(btn).attr('class', 'state5');
+	}
+	else if ($(btn).attr('class') == 'state5')
+	{
+		$(btn).attr('class', 'state6');
+	}
+	else if ($(btn).attr('class') == 'state6')
+	{
+		$(btn).attr('class', 'state7');
+	}
+	else if ($(btn).attr('class') == 'state7')
+	{
+		$(btn).attr('class', 'state8');
+	}
+	else if ($(btn).attr('class') == 'state8')
 	{
 		$(btn).attr('class', 'state0');
 	}
@@ -56,22 +86,67 @@ function saveRules()
 	{
 		var rule = [0, 0, 0, 0];
 		
+		// get target cell type
 		if ($(this).find('#btnTarget').attr('class') == 'state0')
 			rule[0] = Number(0);
 		else if ($(this).find('#btnTarget').attr('class') == 'state1')
 			rule[0] = Number(1);
+		else if ($(this).find('#btnTarget').attr('class') == 'state2')
+			rule[0] = Number(2);
+		else if ($(this).find('#btnTarget').attr('class') == 'state3')
+			rule[0] = Number(3);
+		else if ($(this).find('#btnTarget').attr('class') == 'state4')
+			rule[0] = Number(4);
+		else if ($(this).find('#btnTarget').attr('class') == 'state5')
+			rule[0] = Number(5);
+		else if ($(this).find('#btnTarget').attr('class') == 'state6')
+			rule[0] = Number(6);
+		else if ($(this).find('#btnTarget').attr('class') == 'state7')
+			rule[0] = Number(7);
+		else if ($(this).find('#btnTarget').attr('class') == 'state8')
+			rule[0] = Number(8);
 
 		rule[1] = ($(this).find('#ddlSurround option:selected').text());
 
+		// get surrounding cell type
 		if ($(this).find('#btnSurround').attr('class') == 'state0')
 			rule[2] = Number(0);
 		else if ($(this).find('#btnSurround').attr('class') == 'state1')
 			rule[2] = Number(1);
+		else if ($(this).find('#btnSurround').attr('class') == 'state2')
+			rule[2] = Number(2);
+		else if ($(this).find('#btnSurround').attr('class') == 'state3')
+			rule[2] = Number(3);
+		else if ($(this).find('#btnSurround').attr('class') == 'state4')
+			rule[2] = Number(4);
+		else if ($(this).find('#btnSurround').attr('class') == 'state5')
+			rule[2] = Number(5);
+		else if ($(this).find('#btnSurround').attr('class') == 'state6')
+			rule[2] = Number(6);
+		else if ($(this).find('#btnSurround').attr('class') == 'state7')
+			rule[2] = Number(7);
+		else if ($(this).find('#btnSurround').attr('class') == 'state8')
+			rule[2] = Number(8);
 
+		// get resulting cell type
 		if ($(this).find('#btnResult').attr('class') == 'state0')
 			rule[3] = Number(0);
 		else if ($(this).find('#btnResult').attr('class') == 'state1')
 			rule[3] = Number(1);
+		else if ($(this).find('#btnResult').attr('class') == 'state2')
+			rule[3] = Number(2);
+		else if ($(this).find('#btnResult').attr('class') == 'state3')
+			rule[3] = Number(3);
+		else if ($(this).find('#btnResult').attr('class') == 'state4')
+			rule[3] = Number(4);
+		else if ($(this).find('#btnResult').attr('class') == 'state5')
+			rule[3] = Number(5);
+		else if ($(this).find('#btnResult').attr('class') == 'state6')
+			rule[3] = Number(6);
+		else if ($(this).find('#btnResult').attr('class') == 'state7')
+			rule[3] = Number(7);
+		else if ($(this).find('#btnResult').attr('class') == 'state8')
+			rule[3] = Number(8);
 
 		rulesToSave.ruleset.push(rule);
 	});
